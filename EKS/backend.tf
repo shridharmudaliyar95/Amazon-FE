60f98f7ba6dc3808ef9740e318ecffcdf47bd878
Shridhar B <71603353+shridharmudaliyar95@users.noreply.github.com>
2024-05-07 00:14:31 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ravi-project-s3bucket"  # Replace with your actual S3 bucket name
+    bucket = "megha-bucket-95"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
-    region = "ap-south-1" 
+    region = "ap-southeast-1" 
   }
 }
